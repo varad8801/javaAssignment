3fa6beaee18a37a24243e1c0beda25e6144cdeee
varad <sardeshpandevarad2001@gmail.com>
2023-09-29 17:37:26 +0530

    second

--- src/Q3.java
+++ src/Q3.java
@@ -1,9 +1,41 @@
-
 public class Q3 {
+    public static void main(String[] args) {
+        // Demonstrating int data type
+        int num1 = 10;
+        int num2 = 5;
+        int sumInt = num1 + num2;
+        int differenceInt = num1 - num2;
+        int productInt = num1 * num2;
+        int quotientInt = num1 / num2;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println("int Arithmetic Operations:");
+        System.out.println("Sum: " + sumInt);
+        System.out.println("Difference: " + differenceInt);
+        System.out.println("Product: " + productInt);
+        System.out.println("Quotient: " + quotientInt);
+        System.out.println();
 
-	}
+        // Demonstrating double data type
+        double num3 = 10.0;
+        double num4 = 3.0;
+        double sumDouble = num3 + num4;
+        double differenceDouble = num3 - num4;
+        double productDouble = num3 * num4;
+        double quotientDouble = num3 / num4;
 
+        System.out.println("double Arithmetic Operations:");
+        System.out.println("Sum: " + sumDouble);
+        System.out.println("Difference: " + differenceDouble);
+        System.out.println("Product: " + productDouble);
+        System.out.println("Quotient: " + quotientDouble);
+        System.out.println();
+
+        // Demonstrating String data type
+        String str1 = "Hello";
+        String str2 = " World!";
+        String concatenation = str1 + str2;
+
+        System.out.println("String Concatenation:");
+        System.out.println(concatenation);
+    }
 }
